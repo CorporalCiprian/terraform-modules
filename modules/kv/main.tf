988d9c902d783413f565baf26ba6ceaa9daeebd5
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-08-21 12:27:29 +0300

    added kv config

---
+++ modules/kv/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_key_vault" "kv_module" {
+  name                       = "kv-${var.project_name}-${var.env}v"
+  location                   = var.location
+  resource_group_name        = var.rgname
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  rbac_authorization_enabled = true
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+    ip_rules = [
+      "136.255.102.82/32",
+    ]
+  } 
+}
---
+++ modules/kv/variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+    type=string
+    default = "todo"
+}
+
+variable "env" {
+  type = string
+  default = "dev"
+}
+
+variable "rgname" {
+  type=string
+}
+
+variable "location" {
+  type = string
+}

46b5683105bf6716ab4edca22b0fcc67a82e13bc
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-08-21 13:43:16 +0300

    added kv config

--- modules/kv/main.tf
+++ modules/kv/main.tf
@@ -1,8 +1,9 @@
+data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "kv_module" {
   name                       = "kv-${var.project_name}-${var.env}v"
   location                   = var.location
   resource_group_name        = var.rgname
-  tenant_id                  = var.tenant_id
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 7
   rbac_authorization_enabled = true

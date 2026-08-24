b98b4417443e7e712c7e0448af35509a9ca8890d
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-08-24 12:08:08 +0300

    fix kv inputs

--- modules/kv/main.tf
+++ modules/kv/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_key_vault" "kv" {
   location                   = var.location
   resource_group_name        = var.rgname
   tenant_id                  = data.azurerm_client_config.current.tenant_id
-  sku_name                   = "standard"
+  sku_name                   = var.sku
   soft_delete_retention_days = 7
   rbac_authorization_enabled = true
   network_acls {

593df315bf20eb594a26359ac279deace3149bfd
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-08-27 11:59:13 +0300

    ..

--- modules/virtualnetworking/NSG/main.tf
+++ modules/virtualnetworking/NSG/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_security_group" "nsg" {
     name = var.name
     location = var.location
-    resource_group_name = var.rgname
+    resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_network_security_rule" "rule" {

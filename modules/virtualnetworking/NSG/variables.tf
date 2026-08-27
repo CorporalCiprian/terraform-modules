c352763701eef67ccd0c32d030e9de39d51f92b6
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-08-27 12:13:38 +0300

    ..

--- modules/virtualnetworking/NSG/variables.tf
+++ modules/virtualnetworking/NSG/variables.tf
@@ -13,21 +13,22 @@ variable "resource_group_name" {
 variable "security_rules" {
   type = map(object({
     name = string
+    resource_group_name = string
     protocol = string
     description = optional(string)
     source_port_range = optional(string)
-    source_port_ranges = optional(string)
+    source_port_ranges = optional(list(string))
 
     destination_port_range = optional(string)
-    destination_port_ranges = optional(string)
+    destination_port_ranges = optional(list(string))
 
     source_address_prefix = optional(string)
-    source_address_prefixes = optional(string)
-    source_application_security_group_ids = optional(string)
+    source_address_prefixes = optional(list(string))
+    source_application_security_group_ids = optional(list(string))
 
     destination_address_prefix = optional(string)
-    destination_address_prefixes = optional(string)
-    destination_application_security_group_ids = optional(string)
+    destination_address_prefixes = optional(list(string))
+    destination_application_security_group_ids = optional(list(string))
 
     access = string
     priority = string

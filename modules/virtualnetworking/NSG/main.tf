b58f346198745667ca38f42895539de010972af4
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-08-27 12:07:20 +0300

    ..

--- modules/virtualnetworking/NSG/main.tf
+++ modules/virtualnetworking/NSG/main.tf
@@ -6,28 +6,28 @@ resource "azurerm_network_security_group" "nsg" {
 
 resource "azurerm_network_security_rule" "rule" {
   for_each = var.security_rules
-  name = each.name
-  resource_group_name = each.resource_group_name
-  protocol = each.protocol
-  description = each.description
+  name = each.value.name
+  resource_group_name = each.value.resource_group_name
+  protocol = each.value.protocol
+  description = each.value.description
 
-  source_port_range = each.source_port_range
-  source_port_ranges = each.source_port_ranges
+  source_port_range = each.value.source_port_range
+  source_port_ranges = each.value.source_port_ranges
 
-  destination_port_range = each.destination_port_range
-  destination_port_ranges = each.destination_port_ranges
+  destination_port_range = each.value.destination_port_range
+  destination_port_ranges = each.value.destination_port_ranges
 
-  source_address_prefix = each.source_address_prefix
-  source_address_prefixes = each.source_address_prefixes
-  source_application_security_group_ids = each.source_application_security_group_ids
+  source_address_prefix = each.value.source_address_prefix
+  source_address_prefixes = each.value.source_address_prefixes
+  source_application_security_group_ids = each.value.source_application_security_group_ids
 
-  destination_address_prefix = each.destination_address_prefix
-  destination_address_prefixes = each.destination_address_prefixes
-  destination_application_security_group_ids = each.destination_application_security_group_ids
+  destination_address_prefix = each.value.destination_address_prefix
+  destination_address_prefixes = each.value.destination_address_prefixes
+  destination_application_security_group_ids = each.value.destination_application_security_group_ids
 
-  access = each.access
-  priority = each.priority
-  direction = each.direction
+  access = each.value.access
+  priority = each.value.priority
+  direction = each.value.direction
   network_security_group_name = azurerm_network_security_group.nsg.name
 
 }

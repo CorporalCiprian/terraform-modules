cd588171b51b52daae94654a7a2d81b2df8be204
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-08-27 11:40:03 +0300

    ..

---
+++ modules/virtualnetworking/NSG/variables.tf
@@ -0,0 +1,37 @@
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "rgname" {
+  type = string
+}
+
+variable "security_rules" {
+  type = map(object({
+    name = string
+    resource_group_name = string
+    protocol = string
+    description = optional(string)
+    source_port_range = (string)
+    source_port_ranges = optional(string)
+
+    destination_port_range = optional(string)
+    destination_port_ranges = optional(string)
+
+    source_address_prefix = optional(string)
+    source_address_prefixes = optional(string)
+    source_application_security_group_ids = optional(string)
+
+    destination_address_prefix = optional(string)
+    destination_address_prefixes = optional(string)
+    destination_application_security_group_ids = optional(string)
+    
+    access = string
+    priority = string
+    direction = string
+  }))
+}

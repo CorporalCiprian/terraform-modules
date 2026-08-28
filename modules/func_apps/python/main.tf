0c96d5e277b5ee442a5208dde1146991836368f8
CorporalCiprian <cipriangheorghe197870@gmail.com>
2026-08-28 11:25:38 +0300

    .

--- modules/func_apps/python/main.tf
+++ modules/func_apps/python/main.tf
@@ -19,14 +19,14 @@ resource "azurerm_linux_function_app" "func_app"{
       python_version = var.site_config.python_version
       node_version = var.site_config.node_version 
     }
-     always_on = optional(bool,false)
-     api_definition_url = optional(string)
-     api_management_api_id = optional(string)
-     app_command_line = optional(string)
-     app_scale_limit = optional(string)
-     application_insights_connection_string = optional(string)
-     application_insights_key = optional(string)
-     vnet_route_all_enabled = optional(bool,false)
+     always_on = var.site_config.always_on
+     api_definition_url = var.site_config.api_definition_url
+     api_management_api_id = var.site_config.api_management_api_id
+     app_command_line = var.site_config.app_command_line
+     app_scale_limit = var.site_config.app_scale_limit
+     application_insights_connection_string = var.site_config.application_insights_connection_string
+     application_insights_key = var.site_config.application_insights_key
+     vnet_route_all_enabled = var.site_config.vnet_route_all_enabled
     
     dynamic "ip_restriction" {
       for_each = var.ip_restrictions
